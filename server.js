8d409cd44fb6f1122f565b4340fb4483c2155174
Tanachi <tanachige@gmail.com>
2016-07-06 21:37:57 -1000

    Able to change the index file

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ req.on('data', function(chunk){
   var elementNumber = '';
   var elementName = '';
   var elementDesc = '';
+  var elementList = '';
+  var elementCount = -1;
   var stuff = chunk.toString().split('&');
   stuff.forEach(function(element, index, value){
 
@@ -25,6 +27,7 @@ req.on('data', function(chunk){
     }
 
   });
+  elementCount++;
   var elementPage ='<!DOCTYPE html>\n' +
   '<html lang="en">\n' +
   '<head>\n' +
@@ -40,10 +43,48 @@ req.on('data', function(chunk){
     '<p><a href="/">back</a></p>\n' +
   '</body>\n' +
   '</html>';
+
+
   fs.writeFile('./public/'+elementName + '.html', elementPage, 'utf8', function(err){
     if(err)
       throw err;
   });
+
+  fs.readdir('./public', function(err, files){
+    var totalList = '';
+    files.forEach(function(element, index, array){
+      var linkSplit = element.split('.');
+      if(linkSplit.length > 1){
+        if(linkSplit[0] !== '404' && linkSplit[0] !== 'index' && linkSplit[1] !== 'keep'){
+          elementCount++;
+          totalList += '<li>\n' +
+          '<a href="/' + linkSplit[0] + '.html">' + linkSplit[0] +'</a>\n' +
+          '</li>\n';
+        }
+      }
+    });
+    var indexPage = '<!DOCTYPE html>\n' +
+    '<html lang="en">\n' +
+    '<head>\n' +
+    '<meta charset="UTF-8">\n' +
+    '<title>The Elements</title>\n' +
+    '<link rel="stylesheet" href="/css/styles.css">\n' +
+    '</head>\n' +
+    '<body>\n' +
+    '<h1>The Elements</h1>\n' +
+    '<h2>These are all the known elements.</h2>\n' +
+    '<h3>These are ' + elementCount +'</h3>\n' +
+    '<ol>\n' +
+    totalList +
+    '</ol>\n' +
+    '</body\n>' +
+    '</html>';
+    fs.writeFile('./public/index.html', indexPage, 'utf8', function(err){
+      if(err)
+        throw err;
+    });
+  });
+
   res.end();
 });
 
