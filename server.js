a9ef301329c593ab2bec7dad796acb4e9a505a9c
Tanachi <tanachige@gmail.com>
2016-07-06 19:38:27 -1000

    able to display css files

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function (request, response){
-  fs.readFile('./public/index.html', 'utf8', function(err, data){
-    if(err)
-      throw err;
-    response.write(data);
-    response.end();
-  });
+var server = http.createServer(function (req, res){
+
+
 });
+
+
 
 server.on('clientError', function (err, socket){
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(8000);
+
+server.on('request', function(req, res){
+  if(req.method === 'GET'){
+    if(req.url === '/') {
+      fs.readFile('./public/index.html', 'utf8', function(err, data){
+        if(err)
+          throw err;
+        res.write(data);
+        res.end();
+      });
+    }
+    else{
+      fs.readFile('./public' + req.url, 'utf8', function(err, data){
+        if(err)
+          throw err;
+        res.write(data);
+        res.end();
+      });
+    }
+  }
+});
+server.listen(8080, '127.0.0.1' ,  function(){
+
+});
+
