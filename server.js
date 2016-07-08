450a2ae8b5816e215a2f9004b14ceba8ff8beac6
Tanachi <tanachige@gmail.com>
2016-07-07 18:26:22 -1000

    File checking for Post

--- server.js
+++ server.js
@@ -1,93 +1,117 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req, res){
-req.on('data', function(chunk){
-  var elementSymbol = '';
-  var elementNumber = '';
-  var elementName = '';
-  var elementDesc = '';
-  var elementList = '';
-  var elementCount = -1;
-  var stuff = chunk.toString().split('&');
-  stuff.forEach(function(element, index, value){
+  if(req.method === 'POST'){
+    req.on('data', function(chunk){
+      var elementSymbol = '';
+      var elementNumber = '';
+      var elementName = '';
+      var elementDesc = '';
+      var elementList = '';
+      var elementCount = -1;
+      var stuff = chunk.toString().split('&');
+      stuff.forEach(function(element, index, value){
 
-    var keySplit = element.split('=');
-    if(keySplit[0] === 'elementName'){
-      elementName = keySplit[1];
-    }
-    if(keySplit[0] === 'elementSymbol'){
-      elementSymbol = keySplit[1];
-    }
-    if(keySplit[0] === 'elementAtomicNumber'){
-      elementNumber = keySplit[1];
-    }
-    if(keySplit[0] === 'elementDescription'){
-      var plusSplit = keySplit[1].split('+');
-      elementDesc= plusSplit.join(' ');
-    }
-
-  });
-  elementCount++;
-  var elementPage ='<!DOCTYPE html>\n' +
-  '<html lang="en">\n' +
-  '<head>\n' +
-    '<meta charset="UTF-8">\n' +
-    '<title>The Elements - '+ elementName +'</title>\n' +
-    '<link rel="stylesheet" href="/css/styles.css">\n' +
-  '</head>\n' +
-  '<body>\n' +
-    '<h1>'+ elementName + '</h1>\n' +
-    '<h2>'+ elementSymbol + '</h2>\n' +
-    '<h3>Atomic number '+ elementNumber + '</h3>\n' +
-    '<p>' + elementDesc + '</p>\n' +
-    '<p><a href="/">back</a></p>\n' +
-  '</body>\n' +
-  '</html>';
-
-
-  fs.writeFile('./public/'+elementName + '.html', elementPage, 'utf8', function(err){
-    if(err)
-      throw err;
-  });
-
-  fs.readdir('./public', function(err, files){
-    var totalList = '';
-    files.forEach(function(element, index, array){
-      var linkSplit = element.split('.');
-      if(linkSplit.length > 1){
-        if(linkSplit[0] !== '404' && linkSplit[0] !== 'index' && linkSplit[1] !== 'keep'){
-          elementCount++;
-          totalList += '<li>\n' +
-          '<a href="/' + linkSplit[0] + '.html">' + linkSplit[0] +'</a>\n' +
-          '</li>\n';
+        var keySplit = element.split('=');
+        if(keySplit[0] === 'elementName'){
+          elementName = keySplit[1];
         }
-      }
-    });
-    var indexPage = '<!DOCTYPE html>\n' +
-    '<html lang="en">\n' +
-    '<head>\n' +
-    '<meta charset="UTF-8">\n' +
-    '<title>The Elements</title>\n' +
-    '<link rel="stylesheet" href="/css/styles.css">\n' +
-    '</head>\n' +
-    '<body>\n' +
-    '<h1>The Elements</h1>\n' +
-    '<h2>These are all the known elements.</h2>\n' +
-    '<h3>These are ' + elementCount +'</h3>\n' +
-    '<ol>\n' +
-    totalList +
-    '</ol>\n' +
-    '</body\n>' +
-    '</html>';
-    fs.writeFile('./public/index.html', indexPage, 'utf8', function(err){
-      if(err)
-        throw err;
-    });
-  });
+        if(keySplit[0] === 'elementSymbol'){
+          elementSymbol = keySplit[1];
+        }
+        if(keySplit[0] === 'elementAtomicNumber'){
+          elementNumber = keySplit[1];
+        }
+        if(keySplit[0] === 'elementDescription'){
+          var plusSplit = keySplit[1].split('+');
+          elementDesc= plusSplit.join(' ');
+        }
 
-  res.end();
-});
+      });
 
+      fs.access('./public/' + elementName.toLowerCase() + '.html', fs.F_OK, function(err){
+        if(!err){
+          res.writeHead(400);
+          res.end();
+        }
+        else{
+          elementCount++;
+          var elementPage ='<!DOCTYPE html>\n' +
+          '<html lang="en">\n' +
+          '<head>\n' +
+            '<meta charset="UTF-8">\n' +
+            '<title>The Elements - '+ elementName.charAt(0).toUpperCase() + elementName.slice(1) +'</title>\n' +
+            '<link rel="stylesheet" href="/css/styles.css">\n' +
+          '</head>\n' +
+          '<body>\n' +
+            '<h1>'+ capitalizeFirstLetter(elementName) + '</h1>\n' +
+            '<h2>'+ elementSymbol + '</h2>\n' +
+            '<h3>Atomic number '+ elementNumber + '</h3>\n' +
+            '<p>' + elementDesc + '</p>\n' +
+            '<p><a href="/">back</a></p>\n' +
+          '</body>\n' +
+          '</html>';
+
+
+          fs.writeFile('./public/'+elementName + '.html', elementPage, 'utf8', function(err){
+            if(err)
+              throw err;
+          });
+
+          fs.readdir('./public', function(err, files){
+            var totalList = '';
+            files.forEach(function(element, index, array){
+            var linkSplit = element.split('.');
+            if(linkSplit.length > 1){
+              if(linkSplit[0] !== '404' && linkSplit[0] !== 'index' && linkSplit[1] !== 'keep'){
+                elementCount++;
+                totalList += '<li>\n' +
+                '<a href="/' + linkSplit[0] + '.html">' + linkSplit[0].charAt(0).toUpperCase() + linkSplit[0].slice(1) +'</a>\n' +
+                '</li>\n';
+              }
+            }
+          });
+            var indexPage = '<!DOCTYPE html>\n' +
+            '<html lang="en">\n' +
+            '<head>\n' +
+            '<meta charset="UTF-8">\n' +
+            '<title>The Elements</title>\n' +
+            '<link rel="stylesheet" href="/css/styles.css">\n' +
+            '</head>\n' +
+            '<body>\n' +
+            '<h1>The Elements</h1>\n' +
+            '<h2>These are all the known elements.</h2>\n' +
+            '<h3>These are ' + elementCount +'</h3>\n' +
+            '<ol>\n' +
+            totalList +
+            '</ol>\n' +
+            '</body\n>' +
+            '</html>';
+            fs.writeFile('./public/index.html', indexPage, 'utf8', function(err){
+              if(err)
+                throw err;
+            });
+        });
+
+      res.end();
+        }
+      });
+    });
+  }
+  if(req.method === 'PUT'){
+    req.on('data', function(chunk){
+      console.log(chunk.toString());
+    });
+    res.end();
+  }
+
+  if(req.method === 'DELETE'){
+
+  }
+
+  if(req.method === 'GET'){
+
+  }
 });
 
 
@@ -120,3 +144,15 @@ server.listen(8080, '127.0.0.1' ,  function(){
 
 });
 
+
+function writeElement(){
+
+}
+
+function writeIndex(){
+
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
